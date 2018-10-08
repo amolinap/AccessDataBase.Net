764a1241a5461bfa6cbc110166c7481906084b7d
Alejandro Molina Pulido <am.alex09@gmail.com>
2018-10-08 12:00:52 -0500

    Se crea clase Oracle para probar la conexion a una base de datos Oracle XE 11g

---
+++ Libs/Oracle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using Oracle.DataAccess.Client;
+
+namespace DataBaseNet
+{
+    public class Oracle
+    {
+        private OracleConnection connection;
+        private OracleCommand command;
+        private OracleTransaction transaction;
+
+        public Oracle(string user, string pass, string db_name, string server, int port = 3306)
+        {
+            connection = new OracleConnection("Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = "
+                 + server + ")(PORT = " + port + "))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = "
+                 + db_name + ")));Password=" + pass + ";User ID=" + user + ";");
+
+            transaction = null;
+            command = new OracleCommand();
+        }
+
+        public Oracle(string connection_string)
+        {
+            connection = new OracleConnection(connection_string);
+            transaction = null;
+            command = new OracleCommand();
+        }
+    }
+}
