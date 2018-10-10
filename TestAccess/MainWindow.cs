f4f64b10fb7fced758925ea4931a3b0b9a4c1411
Alejandro Molina Pulido <am.alex09@gmail.com>
2018-10-10 14:23:13 -0500

    Se renombre Form.cs a MainWindow.cs

--- TestAccess/MainWindow.cs
+++ TestAccess/MainWindow.cs
@@ -12,9 +12,9 @@ using DataBaseNet;
 
 namespace DataBasesTest
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
